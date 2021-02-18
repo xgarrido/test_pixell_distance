3b90f8f43e92dcbce92fa844e1792fc4060fa833
Xavier Garrido <xavier.garrido@gmail.com>
2021-02-18 19:08:31 +0100

    add apodization

--- test_distance.py
+++ test_distance.py
@@ -5,31 +5,39 @@ import healpy as hp
 import numpy as np
 from pixell import enmap, utils
 
+nholes = 100
+hole_radius_arcmin = 10
+apo_radius_degree = 1
+
 
 def generate_distance_healpix():
     np.random.seed(1234)
-    nside = 128
+    nside = 256
     m = np.ones(12 * nside ** 2)
     idx = np.arange(12 * nside ** 2)
-    for i in range(100):
+    for i in range(nholes):
         vec = hp.pix2vec(nside, np.random.choice(idx))
-        disc = hp.query_disc(nside, vec, 5 / (60.0 * 180) * np.pi)
+        disc = hp.query_disc(nside, vec, hole_radius_arcmin / (60.0 * 180) * np.pi)
         m[disc] = 0.0
-    dist = enmap.distance_transform_healpix(m)
-    m[dist * 60 * 180 / np.pi < 5] = 0.0
-    return np.asarray(m)
+    dist = enmap.distance_transform_healpix(m) * 180 / np.pi
+    idx = np.where(dist > apo_radius_degree)
+    m = 1 / 2 * (1 - np.cos(-np.pi * dist / apo_radius_degree))
+    m[idx] = 1.0
+    return np.asarray(m)[:1000]
 
 
 def generate_distance_car():
     np.random.seed(1234)
-    shape, wcs = enmap.fullsky_geometry(res=20 * utils.arcmin, proj="car")
+    shape, wcs = enmap.fullsky_geometry(res=10 * utils.arcmin, proj="car")
     m = enmap.ones(shape=shape, wcs=wcs)
-    idx1 = np.random.randint(0, shape[0], 100)
-    idx2 = np.random.randint(0, shape[1], 100)
+    idx1 = np.random.randint(0, shape[0], nholes)
+    idx2 = np.random.randint(0, shape[1], nholes)
     m[idx1, idx2] = 0.0
     dist = enmap.distance_transform(m)
-    m[dist * 60 * 180 / np.pi < 5] = 0.0
-    return np.asarray(m)
+    idx = np.where(dist > apo_radius_degree)
+    m = 1 / 2 * (1 - np.cos(-np.pi * dist / apo_radius_degree))
+    m[idx] = 1.0
+    return np.asarray(m)[:500, :500]
 
 
 def store_data():
