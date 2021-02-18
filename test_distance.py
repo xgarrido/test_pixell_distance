8d3000eab086f34f005fba24d470d191b14d8675
Xavier Garrido <xavier.garrido@gmail.com>
2021-02-18 17:53:13 +0100

    fix test name...

--- test_distance.py
+++ test_distance.py
@@ -4,28 +4,39 @@ import unittest
 import numpy as np
 from pixell import enmap, utils
 
-with open("data/distance_healpix.pkl", "rb") as f:
-    ref_healpix = pickle.load(f)
-with open("data/distance_car.pkl", "rb") as f:
-    ref_car = pickle.load(f)
+
+def generate_distance_healpix():
+    nside = 128
+    m = np.ones(12 * nside ** 2)
+    m[6 * nside ** 2] = 0.0
+    dist = enmap.distance_transform_healpix(m)
+    return np.asarray(dist)
+
+
+def generate_distance_car():
+    shape, wcs = enmap.fullsky_geometry(res=20 * utils.arcmin, proj="car")
+    m = enmap.ones(shape=shape, wcs=wcs)
+    m[250, 250] = 0.0
+    dist = enmap.distance_transform(m)
+    return np.asarray(dist)
+
+
+def store_data():
+    d = {"car": generate_distance_car(), "healpix": generate_distance_healpix()}
+    with open("./data/distances.pkl", "wb") as f:
+        pickle.dump(d, f)
 
 
 class DistanceTest(unittest.TestCase):
-    def test_distance_healpix(self):
-        nside = 128
-        m = np.ones(12 * nside ** 2)
-        m[6 * nside ** 2] = 0.0
-        dist = enmap.distance_transform_healpix(m)
-
-        np.testing.assert_almost_equal(ref_healpix, dist, decimal=7)
+    def setUp(self):
+        with open("data/distances.pkl", "rb") as f:
+            self.ref = pickle.load(f)
 
     def test_distance_healpix(self):
-        shape, wcs = enmap.fullsky_geometry(res=20 * utils.arcmin, proj="car")
-        m = enmap.ones(shape=shape, wcs=wcs)
-        m[250, 250] = 0.0
-        dist = enmap.distance_transform(m)
+        np.testing.assert_almost_equal(self.ref["healpix"], generate_distance_healpix(), decimal=7)
 
-        np.testing.assert_almost_equal(ref_car, dist, decimal=7)
+    def test_distance_car(self):
+        np.testing.assert_almost_equal(self.ref["car"], generate_distance_car(), decimal=7)
 
 
 if __name__ == "__main__":
